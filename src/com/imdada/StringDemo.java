e61a522117f16e431ffed3c50960eb490ec9a808
caixinning <caixinning@imdada.cn>
2017-11-09 11:50:45 +0800

    add test

---
+++ src/com/imdada/StringDemo.java
@@ -0,0 +1,12 @@
+package com.imdada;
+
+/**
+ * Created by caixinning on 2017/11/9.
+ */
+public class StringDemo {
+
+    public static void main(String[] args) {
+        String s = "123"+args[0]+args[1];
+        System.out.println(s);
+    }
+}
